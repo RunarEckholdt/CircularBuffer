0c7002c459ebd8f914c7d3b660ddbe269dbbfa3e
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-09-23 18:18:27 +0200

    made class thread safe

--- rbfLab/Ringbuffer.h
+++ rbfLab/Ringbuffer.h
@@ -1,29 +1,29 @@
-
-
-
-
-
 #pragma once
 #include <iostream>
 #include <condition_variable>
 #include <thread>
 #include <mutex>
+
+using namespace std;
+
 template<typename T>
 class Ringbuffer
 {
-private:
-	T* buffer;
-	int capacity;
-	int inn;
-	int out;
-	std::mutex wLock;
 public:
 	Ringbuffer(int capacity);
 	~Ringbuffer();
 	void write(T value);
 	T read();
-	int getInn()const;
-	int getOut()const;
+private:
+	T* buffer;
+	int capacity;
+	int inn = 0;
+	int out = 0;
+	mutex wMutex;
+	mutex rMutex;
+	condition_variable wCv;
+	condition_variable rCv;
+	
 	bool checkForWriteLock()const;
 	bool checkForReadLock()const;
 };
@@ -33,8 +33,6 @@ public:
 template<typename T>
 inline Ringbuffer<T>::Ringbuffer(int capacity):capacity(capacity){//setter this->capacity til å være capacity
 	buffer = new T[capacity]; //dynamisk array med størrelse capacity
-	inn = 0;
-	out = 0;
 }
 
 //destructor
@@ -42,31 +40,35 @@ template<typename T>
 inline Ringbuffer<T>::~Ringbuffer(){
 	delete[] buffer;
 }
-wLock.lock(); //bare en thread får lov til å bruke write funksjonen av gangen
 
-wLock.unlock();
+
 
 
 //behandler data som skal inn i buffer
 template<typename T>
 inline void Ringbuffer<T>::write(T value){
+	unique_lock<mutex> writeLock(wMutex);
+	while (checkForWriteLock())
+		wCv.wait(writeLock);
 	buffer[inn] = value;
 	inn = (inn + 1) % capacity; //går fra 0 til (capacity -1) til 0 igjen, (altså i en "ring")
+	rCv.notify_one(); 
+	writeLock.unlock();
 }
 
 //leser av data fra buffer
 template<typename T>
 inline T Ringbuffer<T>::read(){
+	unique_lock<mutex> readLock(rMutex);
+	while (checkForReadLock())
+		rCv.wait(readLock);
 	T val = buffer[out];
 	out = (out + 1) % capacity;
+	wCv.notify_one();
+	readLock.unlock();
 	return val;
 }
 
-template<typename T>
-inline int Ringbuffer<T>::getInn() const{return inn;}
-
-template<typename T>
-inline int Ringbuffer<T>::getOut() const{return out;}
 
 template<typename T>
 inline bool Ringbuffer<T>::checkForWriteLock() const{
--- rbfLab/rbfLab.cpp
+++ rbfLab/rbfLab.cpp
@@ -1,27 +1,14 @@
 #include "Ringbuffer.h"
 #include <chrono>
 
-std::condition_variable readCv;
-std::condition_variable writeCv;
-std::mutex writeLock;
-std::mutex readLock;
-std::mutex generateLock;
-
-
-
-
 
 
 //skriver chars fra consoll til ringbuffer
 void writeToBuffer(Ringbuffer<char>* rb, bool* running) {
-    std::unique_lock<std::mutex> threadLock(writeLock);
     while (*running) {
         char ch;
         std::cin.get(ch);//henter neste char fra cin istreamen
-        rb->write(ch); //skriver til ringbufferen
-        readCv.notify_all(); //låser opp alle mutexer koblet til condition variabelen, de vil så skjekke om de kan gå videre
-        while (rb->checkForWriteLock()) //dersom neste index er ringbufferen sin out, thread låses til condiction variabelen notifyer
-            writeCv.wait(threadLock);
+        rb->write(ch);
     }
 }
 
@@ -29,11 +16,8 @@ void writeToBuffer(Ringbuffer<char>* rb, bool* running) {
 //leser fra ringbuffer til konsoll
 void readFromBuffer(Ringbuffer<char>* rb, bool* running) {
     std::string str = "";
-    std::unique_lock<std::mutex> threadLock(readLock);
     while (*running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        while (rb->checkForReadLock())//dersom out har tatt igjen inn, låser thread til condiction variabelen notifyer
-            readCv.wait(threadLock);
         char ch = rb->read();
         std::cout << ch;
         if (ch == ' ' || ch == '\n') //reset str dersom ordet er ferdig/linjeskift
@@ -42,27 +26,23 @@ void readFromBuffer(Ringbuffer<char>* rb, bool* running) {
             str += ch;
         if (str == "exit" || str == "Exit")
             *running = false;
-        writeCv.notify_one();
     }
 }
 
 void generateText(Ringbuffer<char>* rb, bool* running) {
     std::srand(time(NULL)); //gir en seed til random number enginen basert på system time
     char* text = new char[6];
-    std::unique_lock<std::mutex> threadLock(generateLock);
     while (*running) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(30)); //låser threaden i 30 millisekunder
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         for (int i = 0; i < 5; i++) {
-            char ch = char(rand() % 58 + 65); //genererer tall fra 65 - 122, caster til char
+            //char ch = char(rand() % 58 + 65); //genererer ett tall i mengden <0,57>, legger til 65 så mengden blir <65,122>, caster til char
+            char ch = char(rand() % 10 + 48);
             text[i] = ch;
         }
         text[5] = '\n'; //legger til et linjeskift bak de 5 genererte charsa
         
         for (int i = 0; i < 6; i++) {//skriver charsene inn i ringbufferen
             rb->write(text[i]);
-            readCv.notify_all(); //lar read condiction variabelen gå videre for å se om den kan få lese
-            while (rb->checkForWriteLock()) //så lenge neste index er ringbufferen sin out, thread låses til condiction variabelen notifyer
-                writeCv.wait(threadLock);
         }
         
         
