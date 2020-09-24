c9c0a9b750a42320e39c0876891ed9565dee52e2
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-09-24 16:17:44 +0200

    renamed some variables, added a write() override for arrays

--- rbfLab/Ringbuffer.h
+++ rbfLab/Ringbuffer.h
@@ -6,32 +6,43 @@
 
 using namespace std;
 
+
+
 template<typename T>
 class Ringbuffer
 {
 public:
-	Ringbuffer(int capacity);
+	Ringbuffer(int capacity = 10);
 	~Ringbuffer();
 	void write(T value);
+	void write(T arr[], int begin, int end);
 	T read();
 private:
 	T* buffer;
 	int capacity;
-	int inn = 0;
-	int out = 0;
+	int writeIndex;
+	int readIndex;
 	mutex wMutex;
 	mutex rMutex;
 	condition_variable wCv;
 	condition_variable rCv;
-	
+	int nextIndex(int i)const;
 	bool checkForWriteLock()const;
 	bool checkForReadLock()const;
+	
 };
+
+
+template<typename T>
+ostream& operator<<(ostream& os, Ringbuffer<T> rb);
 
 
 //constructor
 template<typename T>
-inline Ringbuffer<T>::Ringbuffer(int capacity):capacity(capacity){//setter this->capacity til å være capacity
+inline Ringbuffer<T>::Ringbuffer(int capacity):
+	capacity(capacity)//setter this->capacity til å være capacity
+	,writeIndex(0)
+	,readIndex(0){
 	buffer = new T[capacity]; //dynamisk array med størrelse capacity
 }
 
@@ -46,39 +57,56 @@ inline Ringbuffer<T>::~Ringbuffer(){
 
 //behandler data som skal inn i buffer
 template<typename T>
-inline void Ringbuffer<T>::write(T value){
+void Ringbuffer<T>::write(T value){
 	unique_lock<mutex> writeLock(wMutex);
-	while (checkForWriteLock())
-		wCv.wait(writeLock);
-	buffer[inn] = value;
-	inn = (inn + 1) % capacity; //går fra 0 til (capacity -1) til 0 igjen, (altså i en "ring")
-	rCv.notify_one(); 
+	while (checkForWriteLock())//hvis nexte writeIndex index er readIndex
+		wCv.wait(writeLock);//stiller seg i kø for å få skrive
+	buffer[writeIndex] = value;
+	writeIndex = nextIndex(writeIndex);
+	rCv.notify_one(); //vekker en neste read tråden i køen til live
 	writeLock.unlock();
 }
 
+template<typename T>
+inline void Ringbuffer<T>::write(T arr[], int begin, int end){
+	for (int i = begin; i < end+1; i++) {
+		write(arr[i]);
+	}
+}
+
+
 //leser av data fra buffer
 template<typename T>
-inline T Ringbuffer<T>::read(){
+T Ringbuffer<T>::read(){
 	unique_lock<mutex> readLock(rMutex);
 	while (checkForReadLock())
 		rCv.wait(readLock);
-	T val = buffer[out];
-	out = (out + 1) % capacity;
-	wCv.notify_one();
+	T val = buffer[readIndex];
+	readIndex = nextIndex(readIndex);
+	wCv.notify_one(); //vekker neste write tråden i køen til live
 	readLock.unlock();
 	return val;
 }
 
 
+
+template<typename T>
+inline int Ringbuffer<T>::nextIndex(int i)const {
+	return (i + 1) % capacity; //går fra 0 til (capacity -1) til 0 igjen, (altså i en "ring")
+}
+
 template<typename T>
 inline bool Ringbuffer<T>::checkForWriteLock() const{
-	return (inn + 1) % capacity == out;
+	return nextIndex(writeIndex) == readIndex;
 }
 
 template<typename T>
 inline bool Ringbuffer<T>::checkForReadLock() const{
-	return out == inn;
+	return readIndex == writeIndex;
 }
 
 
-
+template<typename T>
+ostream& operator<<(ostream& os, Ringbuffer<T>* rb) {
+	return os << rb->read();
+}
--- rbfLab/rbfLab.cpp
+++ rbfLab/rbfLab.cpp
@@ -4,48 +4,43 @@
 
 
 //skriver chars fra consoll til ringbuffer
-void writeToBuffer(Ringbuffer<char>* rb, bool* running) {
-    while (*running) {
+void writeToBuffer(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
+    while (*ptrRunning) {
         char ch;
         std::cin.get(ch);//henter neste char fra cin istreamen
-        rb->write(ch);
+        ptrRb->write(ch);
     }
 }
 
 
 //leser fra ringbuffer til konsoll
-void readFromBuffer(Ringbuffer<char>* rb, bool* running) {
+void readFromBuffer(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
     std::string str = "";
-    while (*running) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        char ch = rb->read();
-        std::cout << ch;
+    while (*ptrRunning) {
+        this_thread::sleep_for(chrono::milliseconds(100));
+        char ch = ptrRb->read();
+        cout << ch;
         if (ch == ' ' || ch == '\n') //reset str dersom ordet er ferdig/linjeskift
             str = "";
         else
             str += ch;
         if (str == "exit" || str == "Exit")
-            *running = false;
+            *ptrRunning = false;
     }
 }
 
-void generateText(Ringbuffer<char>* rb, bool* running) {
-    std::srand(time(NULL)); //gir en seed til random number enginen basert på system time
+void generateText(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
+    srand(time(NULL));
     char* text = new char[6];
-    while (*running) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    while (*ptrRunning) {
+        this_thread::sleep_for(chrono::milliseconds(100));
         for (int i = 0; i < 5; i++) {
-            //char ch = char(rand() % 58 + 65); //genererer ett tall i mengden <0,57>, legger til 65 så mengden blir <65,122>, caster til char
-            char ch = char(rand() % 10 + 48);
+            //char ch = char(rand() % 58 + 65); //genererer ett tall i mengden <0,57>, legger til 65 så mengden blir <65,122>, (ascii for A-z)
+            char ch = char(rand() % 10 + 48); //genererer ett tall i menden <0,9> legger til 48 så mengden blir <48,57> (ascii for 0-9)
             text[i] = ch;
         }
-        text[5] = '\n'; //legger til et linjeskift bak de 5 genererte charsa
-        
-        for (int i = 0; i < 6; i++) {//skriver charsene inn i ringbufferen
-            rb->write(text[i]);
-        }
-        
-        
+        text[5] = '\n';
+        ptrRb->write(text,0,5);
     }
     delete[] text;
 }
@@ -53,15 +48,15 @@ void generateText(Ringbuffer<char>* rb, bool* running) {
 
 int main(){
     int capacity = 10;
-    Ringbuffer<char>* rb = new Ringbuffer<char>(capacity);
+    Ringbuffer<char>* ptrRb = new Ringbuffer<char>(capacity);
     bool running = true;
-    std::thread th1(writeToBuffer, rb,&running); 
-    std::thread th2(readFromBuffer, rb,&running);
-    std::thread th3(generateText, rb, &running);
+    thread th1(writeToBuffer, ptrRb,&running);
+    thread th2(readFromBuffer, ptrRb,&running);
+    thread th3(generateText, ptrRb, &running);
     if (th1.joinable()) th1.join();
     if (th2.joinable()) th2.join();
     if (th3.joinable()) th3.join();
-    delete rb;
+    delete ptrRb;
     return 0;
 }
 
