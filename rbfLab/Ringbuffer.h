def6000ea0518a137f06c675157b206a0d4372e8
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-09-25 15:48:50 +0200

    added more overriders for write() and made a namespace for the class

--- rbfLab/Ringbuffer.h
+++ rbfLab/Ringbuffer.h
@@ -3,110 +3,128 @@
 #include <condition_variable>
 #include <thread>
 #include <mutex>
+#include <vector>
 
-using namespace std;
+namespace rB{
+	template<typename T>
+	class Ringbuffer{
+	public:
+		Ringbuffer(int capacity = 10);
+		~Ringbuffer();
+		void write(T value);
+		void write(T arr[], int begin, int end);
+		void write(std::vector<T> vec, int begin, int end);
+		void write(std::string, int lenght);
+		T read();
+	private:
+		T* buffer;
+		int capacity;
+		int writeIndex;
+		int readIndex;
+		std::mutex wMutex;
+		std::mutex rMutex;
+		std::condition_variable wCv;
+		std::condition_variable rCv;
+		int nextIndex(int i)const;
+		bool checkForWriteLock()const;
+		bool checkForReadLock()const;
+
+	};
+
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& os, Ringbuffer<T>& rb);
+
+
+	//constructor
+	template<typename T>
+	inline Ringbuffer<T>::Ringbuffer(int capacity):
+		capacity(capacity)//setter this->capacity til å være capacity
+		, writeIndex(0)
+		, readIndex(0){
+		buffer = new T[capacity]; //dynamisk allokert array med størrelse capacity
+	}
+
+	//destructor
+	template<typename T>
+	inline Ringbuffer<T>::~Ringbuffer(){
+		delete[] buffer;
+	}
 
 
 
-template<typename T>
-class Ringbuffer
-{
-public:
-	Ringbuffer(int capacity = 10);
-	~Ringbuffer();
-	void write(T value);
-	void write(T arr[], int begin, int end);
-	T read();
-private:
-	T* buffer;
-	int capacity;
-	int writeIndex;
-	int readIndex;
-	mutex wMutex;
-	mutex rMutex;
-	condition_variable wCv;
-	condition_variable rCv;
-	int nextIndex(int i)const;
-	bool checkForWriteLock()const;
-	bool checkForReadLock()const;
-	
+
+	//behandler data som skal inn i buffer
+	template<typename T>
+	void Ringbuffer<T>::write(T value){
+		std::unique_lock<std::mutex> writeLock(wMutex);
+		while(checkForWriteLock())//hvis nexte writeIndex index er readIndex
+			wCv.wait(writeLock);//stiller seg i kø for å få skrive
+		buffer[writeIndex] = value;
+		writeIndex = nextIndex(writeIndex);
+		rCv.notify_one(); //vekker en neste read tråden i køen til live
+		writeLock.unlock();
+	}
+
+	template<typename T>
+	inline void Ringbuffer<T>::write(T arr[], int begin, int end){
+		for(int i = begin; i < end + 1; i++){
+			write(arr[i]);
+		}
+	}
+
+	template<typename T>
+	inline void Ringbuffer<T>::write(std::vector<T> vec, int begin, int end){
+		for(int i = begin; i < end + 1; i++){
+			write(vec[i]);
+		}
+	}
+
+	template<typename T>
+	inline void Ringbuffer<T>::write(std::string s, int lenght){
+		for(int i = 0; i < lenght; i++){
+			write(s[i]);
+		}
+	}
+
+
+	//leser av data fra buffer
+	template<typename T>
+	T Ringbuffer<T>::read(){
+		std::unique_lock<std::mutex> readLock(rMutex);
+		while(checkForReadLock())
+			rCv.wait(readLock);
+		T val = buffer[readIndex];
+		readIndex = nextIndex(readIndex);
+		wCv.notify_one(); //vekker neste write tråden i køen til live
+		readLock.unlock();
+		return val;
+	}
+
+
+
+	template<typename T>
+	inline int Ringbuffer<T>::nextIndex(int i)const{
+		return (i + 1) % capacity; //går fra 0 til (capacity -1) til 0 igjen, (altså i en "ring")
+	}
+
+	template<typename T>
+	inline bool Ringbuffer<T>::checkForWriteLock() const{
+		return nextIndex(writeIndex) == readIndex;
+	}
+
+	template<typename T>
+	inline bool Ringbuffer<T>::checkForReadLock() const{
+		return readIndex == writeIndex;
+	}
+
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& os, Ringbuffer<T>& rb){
+		return os << rb->read();
+	}
 };
 
 
-template<typename T>
-ostream& operator<<(ostream& os, Ringbuffer<T> rb);
 
 
-//constructor
-template<typename T>
-inline Ringbuffer<T>::Ringbuffer(int capacity):
-	capacity(capacity)//setter this->capacity til å være capacity
-	,writeIndex(0)
-	,readIndex(0){
-	buffer = new T[capacity]; //dynamisk array med størrelse capacity
-}
-
-//destructor
-template<typename T>
-inline Ringbuffer<T>::~Ringbuffer(){
-	delete[] buffer;
-}
-
-
-
-
-//behandler data som skal inn i buffer
-template<typename T>
-void Ringbuffer<T>::write(T value){
-	unique_lock<mutex> writeLock(wMutex);
-	while (checkForWriteLock())//hvis nexte writeIndex index er readIndex
-		wCv.wait(writeLock);//stiller seg i kø for å få skrive
-	buffer[writeIndex] = value;
-	writeIndex = nextIndex(writeIndex);
-	rCv.notify_one(); //vekker en neste read tråden i køen til live
-	writeLock.unlock();
-}
-
-template<typename T>
-inline void Ringbuffer<T>::write(T arr[], int begin, int end){
-	for (int i = begin; i < end+1; i++) {
-		write(arr[i]);
-	}
-}
-
-
-//leser av data fra buffer
-template<typename T>
-T Ringbuffer<T>::read(){
-	unique_lock<mutex> readLock(rMutex);
-	while (checkForReadLock())
-		rCv.wait(readLock);
-	T val = buffer[readIndex];
-	readIndex = nextIndex(readIndex);
-	wCv.notify_one(); //vekker neste write tråden i køen til live
-	readLock.unlock();
-	return val;
-}
-
-
-
-template<typename T>
-inline int Ringbuffer<T>::nextIndex(int i)const {
-	return (i + 1) % capacity; //går fra 0 til (capacity -1) til 0 igjen, (altså i en "ring")
-}
-
-template<typename T>
-inline bool Ringbuffer<T>::checkForWriteLock() const{
-	return nextIndex(writeIndex) == readIndex;
-}
-
-template<typename T>
-inline bool Ringbuffer<T>::checkForReadLock() const{
-	return readIndex == writeIndex;
-}
-
-
-template<typename T>
-ostream& operator<<(ostream& os, Ringbuffer<T>* rb) {
-	return os << rb->read();
-}
--- rbfLab/rbfLab.cpp
+++ rbfLab/rbfLab.cpp
@@ -4,7 +4,7 @@
 
 
 //skriver chars fra consoll til ringbuffer
-void writeToBuffer(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
+void writeToBuffer(rB::Ringbuffer<char>* ptrRb, bool* ptrRunning) {
     while (*ptrRunning) {
         char ch;
         std::cin.get(ch);//henter neste char fra cin istreamen
@@ -14,26 +14,26 @@ void writeToBuffer(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
 
 
 //leser fra ringbuffer til konsoll
-void readFromBuffer(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
+void readFromBuffer(rB::Ringbuffer<char>* ptrRb, bool* ptrRunning) {
     std::string str = "";
     while (*ptrRunning) {
-        this_thread::sleep_for(chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         char ch = ptrRb->read();
-        cout << ch;
+        std::cout << ch;
         if (ch == ' ' || ch == '\n') //reset str dersom ordet er ferdig/linjeskift
             str = "";
         else
             str += ch;
-        if (str == "exit" || str == "Exit")
+        if (str == "exit" || str == "Exit") 
             *ptrRunning = false;
     }
 }
 
-void generateText(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
-    srand(time(NULL));
+void generateText(rB::Ringbuffer<char>* ptrRb, bool* ptrRunning) {
+    std::srand(time(NULL));
     char* text = new char[6];
     while (*ptrRunning) {
-        this_thread::sleep_for(chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
         for (int i = 0; i < 5; i++) {
             //char ch = char(rand() % 58 + 65); //genererer ett tall i mengden <0,57>, legger til 65 så mengden blir <65,122>, (ascii for A-z)
             char ch = char(rand() % 10 + 48); //genererer ett tall i menden <0,9> legger til 48 så mengden blir <48,57> (ascii for 0-9)
@@ -48,11 +48,11 @@ void generateText(Ringbuffer<char>* ptrRb, bool* ptrRunning) {
 
 int main(){
     int capacity = 10;
-    Ringbuffer<char>* ptrRb = new Ringbuffer<char>(capacity);
+    rB::Ringbuffer<char>* ptrRb = new rB::Ringbuffer<char>(capacity);
     bool running = true;
-    thread th1(writeToBuffer, ptrRb,&running);
-    thread th2(readFromBuffer, ptrRb,&running);
-    thread th3(generateText, ptrRb, &running);
+    std::thread th1(writeToBuffer, ptrRb,&running);
+    std::thread th2(readFromBuffer, ptrRb,&running);
+    std::thread th3(generateText, ptrRb, &running);
     if (th1.joinable()) th1.join();
     if (th2.joinable()) th2.join();
     if (th3.joinable()) th3.join();
